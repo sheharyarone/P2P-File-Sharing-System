c60a3c91fdb53aeb082ff3896a5798e4e7f0dd98
Muhammad Sheharyar <sheharyarone@gmail.com>
2024-05-21 23:10:34 +0500

    homepage styling

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const HomePage = () => {
   return (
-    <div className="home-page">
-      <h1>Welcome to P2P File Sharing System</h1>
-      <div className="button-container">
-        <button  onClick={() => (window.location.href = "./share")}>Share Files</button>
-        <button  onClick={() => (window.location.href = "./receive")}>Download Files</button>
+    <div className="home-page d-flex flex-column justify-content-center align-items-center vh-100">
+      <h1 className="text-center">Welcome to P2P File Sharing System</h1>
+      <div className="d-flex justify-content-center gap-3 mt-4">
+        <button
+          className="btn btn-primary share-button"
+          onClick={() => (window.location.href = "./share")}
+        >
+          Share Files
+        </button>
+        <button
+          className="btn btn-secondary download-button"
+          onClick={() => (window.location.href = "./receive")}
+        >
+          Download Files
+        </button>
       </div>
     </div>
   );

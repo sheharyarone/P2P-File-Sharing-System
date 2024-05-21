0a30be6e7ca5161beae198f6e3c26cbb7fe29aa2
Muhammad Sheharyar <sheharyarone@gmail.com>
2024-05-22 00:31:56 +0500

    download page ui

--- client/src/pages/DownloadPage.js
+++ client/src/pages/DownloadPage.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-function DownloadPage({fetchUrl}) {
+function DownloadPage({ fetchUrl }) {
   const [files, setFiles] = useState([]);
 
   useEffect(() => {
@@ -9,45 +9,53 @@ function DownloadPage({fetchUrl}) {
 
   const handleRefresh = () => {
     fetch(fetchUrl)
-      .then(response => response.json())
-      .then(data => setFiles(data))
-      .catch(error => console.error('Error fetching files:', error));
+      .then((response) => response.json())
+      .then((data) => setFiles(data))
+      .catch((error) => console.error("Error fetching files:", error));
   };
 
   const handleDownload = (file) => {
-    const downloadLink = document.createElement('a');
+    const downloadLink = document.createElement("a");
     downloadLink.href = file.Link + file.Filename;
     downloadLink.download = file.Filename;
     downloadLink.click();
   };
-  
 
   return (
-    <div className="file-list-container">
-      <h2>List of Available Files</h2> 
-      <table className="file-table">
-        <thead>
-          <tr>
-            <th>Link</th>
-            <th>Filename</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {files.map(file => (
-            <tr k>
-              <td>{file.Link}</td>
-              <td>{file.Filename}</td>
-              <td>
-                <button className="download-button" onClick={() => handleDownload(file)}>Download</button>
-              </td>
+    <div>
+      .
+      <div className="container d-flex flex-column justify-content-center align-items-center">
+        <h2 className="text-center mb-4">List of Available Files</h2>
+        <table className="table table-striped table-bordered file-table">
+          <thead>
+            <tr>
+              <th scope="col">Link</th>
+              <th scope="col">Filename</th>
+              <th scope="col">Actions</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {files.map((file) => (
+              <tr key={file.id}>
+                {" "}
+                {/* Assuming a unique identifier for each file */}
+                <td>{file.Link}</td>
+                <td>{file.Filename}</td>
+                <td>
+                  <button
+                    className="btn btn-primary download-button"
+                    onClick={() => handleDownload(file)}
+                  >
+                    Download
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
-
 
 export default DownloadPage;

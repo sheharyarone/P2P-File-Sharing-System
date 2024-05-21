ecd3c0a0761b34f6b7f8917c68a0bd4b23973f10
Muhammad Sheharyar <sheharyarone@gmail.com>
2024-05-22 04:45:23 +0500

    donwload page table update

--- client/src/pages/DownloadPage.js
+++ client/src/pages/DownloadPage.js
@@ -22,32 +22,38 @@ function DownloadPage({ fetchUrl }) {
   };
 
   return (
-    <div>
+    <div className="mt-5 pt-5">
       .
       <div className="container d-flex flex-column justify-content-center align-items-center">
         <h2 className="text-center mb-4">List of Available Files</h2>
-        <table className=" table-striped table-bordered" >
-          <thead>
-            <tr>
-              {/* <th scope="col">Link</th> */}
-              <th scope="col">Filename</th>
-              <th scope="col">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {files.map((file) => (
-              <tr key={file.id}>
-                {" "}
-                {/* Assuming a unique identifier for each file */}
-                {/* <td>{file.Link}</td> */}
-                <td>{file.Filename}</td>
-                <td>
-                  <button onClick={() => handleDownload(file)}>Download</button>
-                </td>
+        <div>
+          <button className="ms-auto float-end mb-4" onClick={() => handleRefresh()}>
+            Refresh
+          </button>
+          <br></br>
+
+          <table className=" table-striped table-bordered">
+            <thead>
+              <tr>
+                {/* <th scope="col">Link</th> */}
+                <th scope="col">Filename</th>
+                <th scope="col">Actions</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {files.map((file) => (
+                <tr key={file.id}>
+                  <td>{file.Filename}</td>
+                  <td>
+                    <button onClick={() => handleDownload(file)}>
+                      Download
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );

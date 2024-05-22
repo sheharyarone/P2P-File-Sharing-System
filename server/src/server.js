541d3ccc213c00b218847daf221db04dff11bbe9
Muhammad Sheharyar <sheharyarone@gmail.com>
2024-05-22 05:48:54 +0500

    server can accept peer from other system

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,15 @@
 // app.js
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors=require('cors')
-const fileRoutes = require('./routes/files');
+const cors = require("cors");
+const fileRoutes = require("./routes/files");
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cors()); // Enable CORS for all routes
-app.use('/files', fileRoutes);
+app.use("/files", fileRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
